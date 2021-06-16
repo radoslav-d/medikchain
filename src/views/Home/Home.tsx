a064d57bff75ce348e055866c4db54f5d15958c2
radoslav-d <radoslav.dimitrov@sap.com>
2021-06-16 18:52:23 +0300

    Create Home view
    - implement component
    - introduce assets dir with labels

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,3 +1,20 @@
+import { Typography } from '@material-ui/core';
+import { Variant } from '@material-ui/core/styles/createTypography';
+import LABELS from '../../assets/home-screen-texts.json';
+import './Home.css';
+
 export function Home() {
-  return <div>Hello to Medikchain!</div>;
+  return (
+    <div className="home">
+      {LABELS.en.map((label) => (
+        <Typography
+          variant={label.variant as Variant}
+          color="primary"
+          className={label.className}
+        >
+          {label.content}
+        </Typography>
+      ))}
+    </div>
+  );
 }
